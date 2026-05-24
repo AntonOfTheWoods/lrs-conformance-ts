e02cf5426708bd6a46d4fa9be13b7c0fae386520
Anton Melser <anton.melser@outlook.com>
2026-05-24 12:11:04 +0800

    parity work
    Signed-off-by: Anton Melser <anton.melser@outlook.com>

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ export * from "./fixtures/v2_0/documents";
 export * from "./fixtures/v2_0/statements";
 export * from "./registry/builder";
 export * from "./registry/families";
+export * from "./parity/comparison";
 export * from "./specs/migration/v1-v2-delta";
 export * from "./specs/v1_0_3/proof-slice";
 export * from "./specs/v2_0/proof-slice";
