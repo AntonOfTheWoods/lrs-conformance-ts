3da4d100564c809f4a87fdc77e54cb7df3e05aca
Anton Melser <anton.melser@outlook.com>
2026-05-30 20:29:00 +0800

    good stuff
    Signed-off-by: Anton Melser <anton.melser@outlook.com>

--- rewrite4/test/v2_0/4.2.5-Statement-Voiding.ts
+++ rewrite4/test/v2_0/4.2.5-Statement-Voiding.ts
@@ -202,29 +202,24 @@ describe("Statement Lifecycle Requirements (Data 2.3)", () => {
   describe("An LRS SHALL NOT reject a voided statement because it cannot find the ID of the Object of that statement, nor does the LRS have to try to find it. (4.2.4.1 LRS Rejection Cases, XAPI-00016)", function () {
     const nonExistentStatementID = helper.generateUUID();
 
-    it("Shall not reject a voided statement.", function (done) {
-      const context = this;
-      context.timeout(0);
-      const templates = [{ statement: "{{statements.object_statementref}}" }, { verb: "{{verbs.voided}}" }];
-
-      let data = helper.createFromTemplate(templates);
-
-      data = data.statement;
-      data.object.id = nonExistentStatementID;
-
-      request(helper.getEndpointAndAuth())
+    it("Shall not reject a voided statement.", async function () {const context = this;
+context.timeout(0);
+const templates = [{ statement: "{{statements.object_statementref}}" }, { verb: "{{verbs.voided}}" }];
+let data = helper.createFromTemplate(templates);
+data = data.statement;
+data.object.id = nonExistentStatementID;
+request(helper.getEndpointAndAuth())
         .post(helper.getEndpointStatements())
         .headers(helper.addAllHeaders({}))
         .json(data)
         .expect(200)
         .end(function (err: unknown) {
           if (err) {
-            done(err);
+            throw err;
             return;
           }
 
-          done();
-        });
-    });
+          
+        });});
   });
 });
